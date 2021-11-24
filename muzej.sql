827882d2dfd9ff93401d2d510f28438688988f15
TomislavSabolic <37838708+TomislavSabolic@users.noreply.github.com>
2021-11-24 23:29:51 +0100

    dodavanje sql

---
+++ muzej.sql
@@ -0,0 +1,29 @@
+drop database if exists muzejpp24;
+create database muzejpp24;
+use muzejpp24;
+
+create table muzej (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    izlozba int not null
+);
+
+create table izlozba ( 
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    djela int not null,
+    sponzor int not null
+);
+
+create table djela (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null
+);
+
+create table kustos (
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11),
+    izlozba int not null
+);
