6bcfebc7a376bc1b108a184b5e368d2154a0a7e2
TomislavSabolic <37838708+TomislavSabolic@users.noreply.github.com>
2021-11-27 15:36:34 +0100

    Dodavanje tablica

--- samostanpp24.sql
+++ samostanpp24.sql
@@ -7,3 +7,17 @@ create table samostan (
     naziv varchar(50) not null,
     svecenici int not null
 );
+
+create table svecenici (
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    poslovi int not null,
+    nadredeni int not null
+);
+
+create table poslovi (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    svecenici int not null
+);
