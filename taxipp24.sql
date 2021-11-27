613f6ed9fce98ac5fc8e3d0f9c8fbca838c46aab
TomislavSabolic <37838708+TomislavSabolic@users.noreply.github.com>
2021-11-27 15:39:10 +0100

    Dodavanje tablica

--- taxipp24.sql
+++ taxipp24.sql
@@ -7,3 +7,28 @@ create table tvrtka (
     naziv varchar(50) not null,
     vozilo int not null
 );
+create table vozilo (
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    vozac int not null
+);
+
+create table vozac ( 
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    putnik int not null
+);
+
+create table putnik ( 
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    voznja int not null 
+);
+
+create table voznja (
+    sifra int not null primary key auto_increment,
+    vrijemeVoznje datetime,
+    cijenaVoznje decimal(18,2)
+);
