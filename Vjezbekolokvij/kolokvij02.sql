2d7eaaa3c408a8bf099d836d6002baa74b2bbdee
TomislavSabolic <37838708+TomislavSabolic@users.noreply.github.com>
2021-12-06 13:34:03 +0100

    update zadataka

--- Vjezbekolokvij/kolokvij02.sql
+++ Vjezbekolokvij/kolokvij02.sql
@@ -79,3 +79,87 @@ alter table prijatelj add foreign key(svekar) references svekar(sifra);
 
 alter table decko_zarucnica add foreign key(zarucnica) references zarucnica(sifra);
 alter table decko_zarucnica add foreign key(decko) references decko(sifra);
+
+#1 Unjeti po 3 retka (neprijatelj, cura i decko_zarucnica)
+select * from neprijatelj;
+
+insert into neprijatelj (haljina,modelnaocala,kuna)
+values ('Plava haljina','Ray-Ban',4),
+       ('Zelena haljina','Suncane',5),
+       ('Zuta haljina','Dioptrijske',6);
+
+
+select * from cura;
+
+insert into cura(haljina,drugiputa,majica)
+values  ('Plava haljina','2019-09-11','Plava majica'),
+        ('Zelena haljina','2019-01-21','Zelena majica'),
+        ('Zuta haljina','2012-07-23','Zuta majica');
+        
+
+select * from decko;
+       
+insert into decko(asocijalno)
+values(1),(1),(1);
+
+
+select * from zarucnica;
+       
+insert into zarucnica(bojakose,lipa,indiferentno)
+values  ('plava',99.99,1),
+        ('zelena',88.88,1),
+        ('crna',77.77,1);
+        
+       
+select * from decko_zarucnica;
+
+insert into decko_zarucnica(decko,zarucnica)
+values(3,1),(2,2),(3,3);
+
+
+#2 U tablici prijatelj postavite svim zapisima kolonu treciputa na vrijednost 30. travnja 2020.
+select * from svekar;
+insert into svekar(ogrlica,asocijalno)
+values  (3,1),(3,1),(3,0);
+
+select * from prijatelj;
+insert into prijatelj(treciputa,ekstroventno,svekar)
+values ('2020-12-19',1,1),
+       ('2018-12-19',1,2),
+       ('2019-12-19',1,3);
+       
+update prijatelj set treciputa='2020-04-30';
+
+
+#3 U tablici brat obrišite sve zapise čija je vrijednost kolone ogrlica različito od 14.
+select * from brat;
+insert into brat (ogrlica,asocijalno,neprijatelj)
+values  (14,0,1),
+        (8,1,2),
+        (4,1,3);
+        
+delete from brat where ogrlica != 14;
+
+
+#5 Prikažite novcica iz tablice zarucnica, 
+# neprijatelj iz tablice brat 
+# te haljina iz tablice neprijatelj 
+# uz uvjet da su vrijednosti kolone drugiputa iz tablice cura poznate 
+# te da su vrijednosti kolone vesta iz tablice decko sadrže niz znakova ba. 
+# Podatke posložite po haljina iz tablice neprijatelj silazno.
+select a.novcica , f.neprijatelj , e.haljina
+from zarucnica a 
+inner join decko_zarucnica b on a.sifra = b.zarucnica 
+inner join decko c           on b.decko = c.sifra 
+inner join cura d            on c.sifra = d.decko
+inner join neprijatelj e     on d.sifra = e.cura
+inner join brat f            on e.sifra = f.neprijatelj
+where c.vesta like '%ba%' and d.drugiputa is not null 
+order by e.haljina desc;
+
+
+#6 Prikažite kolone vesta i asocijalno iz tablice decko 
+#  čiji se primarni ključ ne nalaze u tablici decko_zarucnica.
+select a.vesta , a.asocijalno 
+from decko a left join decko_zarucnica b on a.sifra = b.decko 
+where b.decko is null; 
