1854c58b6d31a58cd45649f2bc0d931eee2b0da1
TomislavSabolic <37838708+TomislavSabolic@users.noreply.github.com>
2021-12-08 19:37:08 +0100

    insert unos

--- Vjezbekolokvij/kolokvij03.sql
+++ Vjezbekolokvij/kolokvij03.sql
@@ -80,3 +80,55 @@ alter table ostavljena add foreign key(prijatelj) references prijatelj(sifra);
 
 alter table prijatelj_brat add foreign key(prijatelj) references prijatelj(sifra);
 alter table prijatelj_brat add foreign key(brat) references brat(sifra);
+
+#1 Unjeti po 3 retka (snasa, ostavljena i prijatelj_brat)
+select * from ostavljena;
+
+insert into ostavljena(modelnaocala)
+values  ('Ray-Ban'),
+        ('Suncane'),
+        ('Dioptrijske');
+       
+       
+select * from snasa;
+
+insert into snasa(kuna,eura,ostavljena)
+values  (1.99,10.99,1),
+        (2.33,20.99,2),
+        (3.99,30.99,3);     
+       
+       
+select * from prijatelj;
+
+insert into prijatelj(indiferentno)
+values  (1),(1),(0);
+
+
+select * from brat;
+
+insert into brat(ogrlica, ekstrovertno)
+values  (1,1),
+        (2,0),
+        (3,1);
+       
+       
+select * from prijatelj_brat;
+
+insert into prijatelj_brat(prijatelj,brat)
+values  (1,3), 
+        (2,2), 
+        (3,1);
+        
+       
+#2 U tablici svekar postavite svim zapisima kolonu suknja na vrijednost Osijek.    
+select * from cura;
+insert into cura(ogrlica)
+values (2),(7),(6);
+
+select* from svekar;
+insert into svekar(novcica,suknja,narukvica,cura)
+values  (22.22,'Plava',7,1),
+        (33.11,'Zelena',4,2),
+        (44.55,'Crna',3,3);
+       
+update svekar set suknja = 'Osijek';
